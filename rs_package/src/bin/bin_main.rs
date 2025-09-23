351c71227e3b888dabe5ba3167a1d9bff8c76829
zhenhanyijiu <543058483@qq.com>
2025-09-23 15:28:45 +0800

    add src/bin

---
+++ rs_package/src/bin/bin_main.rs
@@ -0,0 +1,9 @@
+use rs_package::{f33, ut};
+use thirty;
+fn main() {
+    println!("=========== bin ===========");
+    ut::max();
+    ut::mn::min();
+    thirty::function_thirty_package();
+    f33();
+}
